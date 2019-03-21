400fc70d1329d87c689c8f4181aa58e739afd89c
SofiaS1604 <SofiaS1604@yandex.ru>
2019-03-21 15:12:47 +0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require("webpack");
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var devFlagPlugin = new webpack.DefinePlugin({
+  __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || 'false'))
+});
+
+module.exports = {
+    entry: [
+        "webpack-dev-server/client?http://localhost:3000",
+        "webpack/hot/only-dev-server",
+        "./src/index.js",
+    ],
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader",
+                options: { presets: ["@babel/env"] }
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx", ".json"] },
+    output: {
+        path: path.resolve(__dirname, "dist/"),
+        publicPath: "/dist/",
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+        hotOnly: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+};
